de954c07d96a114366f05c0e7dbd9a7f4cf89b53
Smukello <101141116+Smukello@users.noreply.github.com>
2022-10-06 17:08:25 +0200

    Add files via upload

---
+++ test/loginscreen_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:endurance_fitness/loginscreen.dart';
+
+main(){
+  test("Login screen", (){
+    //setup
+
+    //run
+
+    //verify
+
+  });
+
+  test('non-empty email returns null', (){
+    var result = isNull;
+    expect(result, isNotNull);
+  });
+
+  test('password is not empty and its correct', (){
+    var result = isNull;
+    expect(result, isNotNull);
+  });
+
+}
