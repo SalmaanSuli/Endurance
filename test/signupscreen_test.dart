ecc905302822b0b680dcd56d18ad2315979290c1
Smukello <101141116+Smukello@users.noreply.github.com>
2022-10-14 00:54:35 +0200

    Update signupscreen_test.dart

--- test/signupscreen_test.dart
+++ test/signupscreen_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:endurance_fitness/loginscreen.dart';
 import 'package:endurance_fitness/signupscreen.dart';
 
 main(){
@@ -12,19 +11,55 @@ main(){
 
   });
 
-  test('Check if all fields are not null', (){
-    var result = isNull;
-    expect(result, isNotNull);
+  test('empty email', (){
+    var result = EmailFieldValidator.validate('');
+    expect(result, 'Please fill in all fields');
   });
 
-  test('password is not empty and its correct', (){
-    var result = 'Password don\'t match!';
-    expect(result, 'Password don\'t match!');
+  test('empty name field', (){
+    var result = NameFieldValidator.validate('');
+    expect(result, 'Please fill in all fields');
   });
 
-  test('If the password is the same as the confirm password', (){
-    var result = null;
-    expect(result, null);
+  test('short password', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password must be at least 8 characters.');
+  });
+/*
+  test('empty password', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password cannot be empty');
+  });
+
+  test('password with no upper case', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password must have an Uppercase letter.');
+  });
+
+  test('password with no numerical values', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password must contain a digit.');
+  });
+
+  test('password with no lower cases', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password must have an Lowercase letter.');
+  });
+
+  test('password with no special character', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password must contain a special character, like &,#,%.');
+  });
+
+  test('empty password', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'Password cannot be empty');
+  });
+
+  */
+  test('Passwords doesnt match', (){
+    var result = ConfirmPasswordFieldValidator.validate('');
+    expect(result, 'Passwords doesn\'t match');
   });
 
 }
