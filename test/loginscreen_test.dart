32ace3ac6e5492a734a8b213d1eab7c26bec0b8a
Smukello <101141116+Smukello@users.noreply.github.com>
2022-10-14 00:46:13 +0200

    Update loginscreen_test.dart

--- test/loginscreen_test.dart
+++ test/loginscreen_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:endurance_fitness/loginscreen.dart';
 
@@ -11,14 +12,51 @@ main(){
 
   });
 
-  test('non-empty email returns null', (){
-    var result = isNull;
-    expect(result, isNotNull);
+  test('empty or wrong password returns null', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'incorrect password');
   });
 
   test('password is not empty and its correct', (){
-    var result = isNull;
-    expect(result, isNotNull);
+    var result = PasswordFieldValidator.validate('#123abcdeF');
+    expect(result, '');
   });
+
+  test('non-empty email returns null', (){
+    var result = EmailFieldValidator.validate('');
+    expect(result, 'Please fill in all fields');
+  });
+
+    test('non-empty email returns null', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'incorrect password');
+  });
+
+  test('non-empty email returns null', (){
+    var result = PasswordFieldValidator.validate('');
+    expect(result, 'incorrect password');
+  });
+
+  test('Email and password correct', (){
+    var resultEmail = EmailFieldValidator.validate('sal6@gmail.com');
+    var resultPass = PasswordFieldValidator.validate('#123abcdeF');
+    var result = resultEmail+resultPass;
+    expect(result, '');
+  });
+/*
+  Widget makeTestableWidget({required Widget child}){
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('', (WidgetTester tester) async {
+    LoginWidget page = LoginWidget(onSignedIn: () {});
+
+    await tester.pumpWidget(makeTestableWidget(child: page));
+
+  });
+
+  */
 
 }
